f43a7b7c32174a72bea993870663f78b410b3b8a
Sam Fares <oalfares@beachbody.com>
2016-03-24 13:59:50 -0700

    folders

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+// local state for this reducer
+export default function(state = null, action) {
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+
+  return state;
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    {title: 'JavaScript The Good Parts', pages:101},
+    {title: 'Internet for HTML', pages: 30},
+    {title: 'C/C++ ', pages: 20},
+    {title: 'Ruby ', pages:4}
+  ]
+}
