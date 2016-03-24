4d1cd8f2b83c3b460023ad6be818ae6fc806d456
Sam Fares <oalfares@beachbody.com>
2016-03-24 10:49:32 -0700

    Add youtube code

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 
+import BookList from '../container/book-list'
+import BookDetail from '../container/book-detail'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
     );
   }
 }
